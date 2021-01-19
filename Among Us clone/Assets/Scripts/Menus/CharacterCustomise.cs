9ab64a388d78223f2617d1170b0841d0da19cb27
Jack Lynham <Jack Lynham>
2021-01-19 16:20:24 +0000

    Colour Menu Fixed

---
+++ Among Us clone/Assets/Scripts/Menus/CharacterCustomise.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterCustomise : MonoBehaviour
+{
+    [SerializeField] Color[] allColors;
+   public Canvas canvas;
+
+    public void SetColors(int colorIndex)
+
+    {
+        PlayerMovement.localPLayer.SetColor(allColors[colorIndex]);
+    }
+    public void CloseMenu()
+    {
+        canvas.GetComponent<Canvas>();
+        canvas.enabled = false;
+    }
+
+    public void OpenMenu()
+    {
+        canvas.GetComponent<Canvas>();
+        canvas.enabled = true;
+    }
+
+    public void NextScene(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+}
